885e84fc7188cae55969b53a602152e0b9db2395
AndrewsDutraDev <dutrapgmdr@gmail.com>
2022-01-16 14:23:10 -0300

    Add: laboratory means month/year

--- laboratorios/index.php
+++ laboratorios/index.php
@@ -12,6 +12,34 @@ if ($_SESSION['cnpj'] == '') {
     {
         $res[]=$r;
     }
+
+    if(isset($_POST['count_exames'])) {
+        $data_start = $_POST['data_start'];
+        $date_start = date_create($data_start);
+        $formated_data_start = date_format($date_start,"d/m/Y");
+
+        $data_end = $_POST['data_end'];
+        $date_end = date_create($data_end);
+        $formated_data_end = date_format($date_end,"d/m/Y");
+
+        $query = "SELECT * FROM exames WHERE cnpj_lab = $cnpj AND (data_exame_db BETWEEN '$data_start' AND '$data_end')";
+
+        $results = mysqli_query($connect, $query);
+        $num_rows_consultas = mysqli_num_rows($results);
+        while($r=mysqli_fetch_object($results))
+        {
+            $resultados[]=$r;
+        }
+    }
+
+    if(isset($_POST['count_exames_anual'])) {
+        $data_year = $_POST['year'];
+        $query_anual = "SELECT * FROM exames WHERE cnpj_lab = $cnpj AND YEAR(data_exame_db) = $data_year";
+
+        $results_year = mysqli_query($connect, $query_anual);
+        $num_rows_consultas_anuais = mysqli_num_rows($results_year);
+        $media_num_rows_consultas_anuais = round(($num_rows_consultas_anuais)/12, 2);
+    }
 }
 ?>
 
@@ -86,6 +114,48 @@ if ($_SESSION['cnpj'] == '') {
                         echo "<div class='text-center m-2'> Não há exames registrados </div>"; 
                     }
                 ?>
+        </div>
+        <div>
+            <div> Consultas por período </div>
+            <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_exames" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                
+                <label>Data inicio: </label>
+                <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_start" id="data" onfocusout="is_empty(this)">
+                
+
+                <label>Data fim: </label>
+                <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_end" id="data" onfocusout="is_empty(this)">
+                <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_exames" value="Visualizar número de consultas" onclick="send_form()">
+                
+            </form>
+            
+            <?php
+                if (isset($formated_data_start)){
+                    echo "<div>O total de consultas no período de $formated_data_start até $formated_data_end foi de $num_rows_consultas exame(s)</div>";
+                }
+                
+            ?>
+
+        </div>
+
+        <div>
+            <div> Consultas anual </div>
+            <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_exames_anual" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                
+                <label>Ano: </label>
+                <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="year" id="data" onfocusout="is_empty(this)">
+                
+                <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_exames_anual" value="Visualizar número de consultas anuais" onclick="send_form()">
+                
+            </form>
+            
+            <?php
+                if (isset($data_year)){
+                    echo "<div>O total de consultas no ano de $data_year foi de $num_rows_consultas_anuais exame(s)</div>";
+                    echo "<div>A média de consultas no ano de $data_year foi de $media_num_rows_consultas_anuais exame(s)</div>";
+                }
+            ?>
+
         </div>
     </div>
 </body>
