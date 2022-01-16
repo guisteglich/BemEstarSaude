4cd27926b6e3859f170287e3b1fa16e58f24c6cb
Mateus Marques <mateusmarquesdc@gmail.com>
2022-01-16 15:54:33 -0300

    Backend, testado e aprovado.

--- pacientes/consultas.php
+++ pacientes/consultas.php
@@ -12,6 +12,36 @@ if(isset($_POST['BuscaConPac'])) {
     }
 }
 
+if(isset($_POST['count_consultas'])) {
+    $data_start = $_POST['data_start'];
+    $date_start = date_create($data_start);
+    $formated_data_start = date_format($date_start,"d/m/Y");
+    $cpf = $_POST['cpf'];
+
+    $data_end = $_POST['data_end'];
+    $date_end = date_create($data_end);
+    $formated_data_end = date_format($date_end,"d/m/Y");
+
+    $query = "SELECT * FROM consultas WHERE (data_consulta_db BETWEEN '$data_start' AND '$data_end') AND cpf_paciente = $cpf";
+
+    $results = mysqli_query($connect, $query);
+    $num_rows_consultas = mysqli_num_rows($results);
+    while($r=mysqli_fetch_object($results))
+    {
+        $resultados[]=$r;
+    }
+}
+
+if(isset($_POST['count_consultas_anual'])) {
+    $data_year = $_POST['year'];
+    $cpf = $_POST['cpf'];
+
+    $query_anual = "SELECT * FROM consultas WHERE YEAR(data_consulta_db) = $data_year AND cpf_paciente = $cpf";
+    $results_year = mysqli_query($connect, $query_anual);
+    $num_rows_consultas_anuais = mysqli_num_rows($results_year);
+    $media_num_rows_consultas_anuais = round(($num_rows_consultas_anuais)/12, 2);
+}
+
 
 ?>
 
@@ -52,6 +82,53 @@ if(isset($_POST['BuscaConPac'])) {
                     }else{
                         echo "<p class='text-red-500'> Nenhum paciente com o CPF inserido foi encontrado </p>"; 
                     }
+                    ?>
+
+                </div>
+
+                <div>
+                    <div> consultas por período </div>
+                    <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_consultas" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <label>Insira o seu CPF:<label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="cpf" id="cpf">
+                        
+                        <label>Data inicio: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_start" id="data" onfocusout="is_empty(this)">
+                        
+
+                        <label>Data fim: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_end" id="data" onfocusout="is_empty(this)">
+                        <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_consultas" value="Visualizar número de consultas" onclick="send_form()">
+                        
+                    </form>
+                    
+                    <?php
+                        if (isset($formated_data_start)){
+                            echo "<div>O total de consultas no período de $formated_data_start até $formated_data_end foi de $num_rows_consultas consulta(s)</div>";
+                        }
+                    ?>
+
+                </div>
+
+                <div>
+                    <div> consultas anual </div>
+                    <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_consultas_anual" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        
+                        <label>Insira o seu CPF:<label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="cpf" id="cpf">
+
+                        <label>Ano: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="year" id="data" onfocusout="is_empty(this)">
+                       
+                        <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_consultas_anual" value="Visualizar número de consultas anuais" onclick="send_form()">
+                        
+                    </form>
+                    
+                    <?php
+                        if (isset($data_year)){
+                            echo "<div>O total de consultas no ano de $data_year foi de $num_rows_consultas_anuais consulta(s)</div>";
+                            echo "<div>A média de consultas no ano de $data_year foi de $media_num_rows_consultas_anuais consulta(s)</div>";
+                        }
                     ?>
 
                 </div>
--- pacientes/exames.php
+++ pacientes/exames.php
@@ -20,6 +20,35 @@ if(isset($_POST['BuscaConPac'])) {
     }
 }
 
+if(isset($_POST['count_exames'])) {
+    $data_start = $_POST['data_start'];
+    $date_start = date_create($data_start);
+    $formated_data_start = date_format($date_start,"d/m/Y");
+    $cpf = $_POST['cpf'];
+
+    $data_end = $_POST['data_end'];
+    $date_end = date_create($data_end);
+    $formated_data_end = date_format($date_end,"d/m/Y");
+
+    $query = "SELECT * FROM exames WHERE (data_exame_db BETWEEN '$data_start' AND '$data_end') AND cpf_paciente = $cpf";
+
+    $results = mysqli_query($connect, $query);
+    $num_rows_exames = mysqli_num_rows($results);
+    while($r=mysqli_fetch_object($results))
+    {
+        $resultados[]=$r;
+    }
+}
+
+if(isset($_POST['count_exames_anual'])) {
+    $data_year = $_POST['year'];
+    $cpf = $_POST['cpf'];
+
+    $query_anual = "SELECT * FROM exames WHERE YEAR(data_exame_db) = $data_year AND cpf_paciente = $cpf";
+    $results_year = mysqli_query($connect, $query_anual);
+    $num_rows_exames_anuais = mysqli_num_rows($results_year);
+    $media_num_rows_exames_anuais = round(($num_rows_exames_anuais)/12, 2);
+}
 
 ?>
 
@@ -41,7 +70,7 @@ if(isset($_POST['BuscaConPac'])) {
                     <form name="BusConPac" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                         <label>Insira o seu CPF:<label>
                         <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="cpf" id="cpf">
-                        <input class='rounded-md w-auto px-4 h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="BuscaConPac" value="Buscar consultas">
+                        <input class='rounded-md w-auto px-4 h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="BuscaConPac" value="Buscar exames">
                     </form>
                     <?php
                     if ($error) {
@@ -61,6 +90,52 @@ if(isset($_POST['BuscaConPac'])) {
                             } 
                         }  
                     }
+                    ?>
+
+                </div>
+                <div>
+                    <div> exames por período </div>
+                    <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_exames" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <label>Insira o seu CPF:<label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="cpf" id="cpf">
+                        
+                        <label>Data inicio: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_start" id="data" onfocusout="is_empty(this)">
+                        
+
+                        <label>Data fim: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="date" name="data_end" id="data" onfocusout="is_empty(this)">
+                        <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_exames" value="Visualizar número de exames" onclick="send_form()">
+                        
+                    </form>
+                    
+                    <?php
+                        if (isset($formated_data_start)){
+                            echo "<div>O total de exames no período de $formated_data_start até $formated_data_end foi de $num_rows_exames exame(s)</div>";
+                        }
+                    ?>
+
+                </div>
+
+                <div>
+                    <div> exames anual </div>
+                    <form class='flex p-10 flex-col w-2/4 bg-white rounded-lg' name="count_exames_anual" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        
+                        <label>Insira o seu CPF:<label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="cpf" id="cpf">
+
+                        <label>Ano: </label>
+                        <input class='border mb-2 border-gray-200 text-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-green-400 px-3 h-9' type="text" name="year" id="data" onfocusout="is_empty(this)">
+                       
+                        <input class='rounded-full w-auto h-9 mt-5 bg-green-400 text-white hover:bg-green-300 cursor-pointer' type="submit" name="count_exames_anual" value="Visualizar número de exames anuais" onclick="send_form()">
+                        
+                    </form>
+                    
+                    <?php
+                        if (isset($data_year)){
+                            echo "<div>O total de exames no ano de $data_year foi de $num_rows_exames_anuais exame(s)</div>";
+                            echo "<div>A média de exames no ano de $data_year foi de $media_num_rows_exames_anuais exame(s)</div>";
+                        }
                     ?>
 
                 </div>
